11f476ecb14a58a25ef8060f7ba537a73ba9b5d3
大滝正和 <msk_z@ptplus.jp>
2024-05-22 20:00:06 +0900

    chore: Update index.html to add stop button for barcode detection

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,47 @@
 window.addEventListener('load', function() {
-  let detectButton = document.getElementById('detect');
+  const video = document.querySelector('.reader-video');
   let footer = document.getElementsByTagName('footer')[0];
+  let detectButton = document.getElementById('detect');
+  let stopButton = document.getElementById('stop');
+  let timerId = null;
 
-    detectButton.addEventListener('click', () => {
-      if (window.BarcodeDetector == undefined) {
-        footer.innerHTML = "Barcode Detection not supported";
-        console.error(footer.innerHTML);
-        return;
-      }
+  if (window.BarcodeDetector == undefined) {
+    footer.innerHTML = "Barcode Detection not supported";
+    console.error(footer.innerHTML);
+    return;
+  }
 
-      (async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: {
-            facingMode: "user"
-          }
-        });
-        const video = document.querySelector('.reader-video');
-        video.srcObject = stream;
-        await video.play();
+  detectButton.addEventListener('click', () => {
 
-        // 検出をQRコードのみに限定する
-        const detector = new window.BarcodeDetector({
-          formats: ['qr_code']
-        });
+    (async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: "user"
+        }
+      });
+      video.srcObject = stream;
+      await video.play();
 
-        // QRコードの検出
-        setInterval(async () => {
-          const detectionList = await detector.detect(video);
-          for(const detected of detectionList) {
-            console.log(detected.rawValue);
-            if(footer.innerHTML == detected.rawValue) continue;
-            footer.innerHTML = detected.rawValue;
-          }
-        }, 500);
-      })();
-    });
+      // 検出をQRコードのみに限定する
+      const detector = new window.BarcodeDetector({
+        formats: ['qr_code']
+      });
 
+      // QRコードの検出
+      timerId = setInterval( async () => {
+        const detectionList = await detector.detect(video);
+        for(const detected of detectionList) {
+          if(footer.innerHTML == detected.rawValue) continue;
+          console.log(detected.rawValue);
+          footer.innerHTML = detected.rawValue;
+        }
+      }, 500);
+    })();
+  });
+
+  stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    video.pause();
+    video.srcObject = null;
+  });
 });
