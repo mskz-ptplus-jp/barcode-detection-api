01e8b270ef6b88ffd194e214155c82a8c2d630f2
大滝正和 <msk_z@ptplus.jp>
2024-05-22 09:39:48 +0900

    chore: Update app.css and index.html for barcode detection implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,39 @@
 window.addEventListener('load', function() {
-    let detectButton = document.getElementById('detect');
-    let image = document.getElementById('image');
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext("2d");
+  let detectButton = document.getElementById('detect');
+  let footer = document.getElementsByTagName('footer')[0];
 
-    image.addEventListener('load', () => {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.drawImage(image, 
-                    0, 0, image.width, image.height,
-                    0, 0, canvas.width, canvas.height);
+    detectButton.addEventListener('click', () => {
+      if (window.BarcodeDetector == undefined) {
+        footer.innerHTML = "Barcode Detection not supported";
+        console.error(footer.innerHTML);
+        return;
+      }
 
-      const scale = canvas.width / image.width;
+      (async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            facingMode: "user"
+          }
+        });
+        const video = document.querySelector('.reader-video');
+        video.srcObject = stream;
+        await video.play();
 
-      console.log('image', image.width, image.height);
+        // 検出をQRコードのみに限定する
+        const detector = new window.BarcodeDetector({
+          formats: ['qr_code']
+        });
 
-      detectButton.addEventListener('click', () => {
-        if (window.BarcodeDetector == undefined) {
-          let footer = document.getElementsByTagName('footer')[0];
-          footer.innerHTML = "Barcode Detection not supported";
-          console.error(footer.innerHTML);
-          return;
-        }
-        let footer = document.getElementsByTagName('footer')[0];
-
-        let barcodeDetector = new BarcodeDetector();
-        barcodeDetector.detect(image)
-          .then(barcodes => {
-            // Draw the boxes on the <canvas>.
-            let ctx = document.getElementById('canvas').getContext("2d");
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "red";
-            for(let i = 0; i < barcodes.length; i++) {
-              ctx.rect(
-                Math.floor(barcodes[i].boundingBox.x * scale), 
-                Math.floor(barcodes[i].boundingBox.y * scale),
-                Math.floor(barcodes[i].boundingBox.width * scale), 
-                Math.floor(barcodes[i].boundingBox.height * scale)
-              );
-              ctx.stroke();
-            }
-            ctx.closePath();
-
-            ctx.beginPath();
-            ctx.strokeStyle = "yellow";
-            for(let i = 0; i < barcodes.length; i++) {
-              ctx.moveTo(Math.floor(barcodes[i].cornerPoints[0].x * scale), 
-                        Math.floor(barcodes[i].cornerPoints[0].y * scale));
-              ctx.lineTo(Math.floor(barcodes[i].cornerPoints[1].x * scale), 
-                        Math.floor(barcodes[i].cornerPoints[1].y * scale));
-              ctx.lineTo(Math.floor(barcodes[i].cornerPoints[2].x * scale), 
-                        Math.floor(barcodes[i].cornerPoints[2].y * scale));
-              ctx.lineTo(Math.floor(barcodes[i].cornerPoints[3].x * scale), 
-                        Math.floor(barcodes[i].cornerPoints[3].y * scale));
-              ctx.lineTo(Math.floor(barcodes[i].cornerPoints[0].x * scale), 
-                        Math.floor(barcodes[i].cornerPoints[0].y * scale));
-              ctx.stroke();
-            }
-            ctx.closePath();
-
-            // Add the barcodes as strings to the <footer>
-            footer.innerHTML = `<p>Detected ${barcodes.length} barcodes</p>`;
-            const ul = document.createElement('ul');
-            for(let i = 0; i < barcodes.length; i++) {
-              const li = document.createElement('li');
-              li.innerHTML = `@ (${barcodes[i].boundingBox.x}, ${barcodes[i].boundingBox.y}),`
-                            + ` size (${barcodes[i].boundingBox.width}x${barcodes[i].boundingBox.height});`
-                            + ` rawValue=${barcodes[i].rawValue}`;
-              ul.appendChild(li);
-            }
-            footer.appendChild(ul);
-          }).catch((e) => {
-            footer.innerHTML = `Boo, Barcode Detection failed: ${e}`;
-            console.error(footer.innerHTML);
-          })
-      });
+        // QRコードの検出
+        setInterval(async () => {
+          const detectionList = await detector.detect(video);
+          for(const detected of detectionList) {
+            console.log(detected.rawValue);
+            if(footer.innerHTML == detected.rawValue) continue;
+            footer.innerHTML = detected.rawValue;
+          }
+        }, 500);
+      })();
     });
 
-    // 画像の再読み込みを強制
-    image.src = image.src;
-  });
+});
